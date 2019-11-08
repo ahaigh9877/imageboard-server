1abcee040e52c01edfd1e3916485f444721a5041
ahaigh9877 <alastairhaigh@me.com>
2019-11-08 11:56:24 +0100

    added post endpoint

--- image/router.js
+++ image/router.js
@@ -4,6 +4,12 @@ const router = new Router();
 
 const Image = require("./model");
 
+router.post("/image", (req, res, next) => {
+  Image.create(req.body)
+    .then(image => res.send(image))
+    .catch(next);
+});
+
 router.get("/image", (req, res, next) => {
   Image.findAll()
     .then(images => res.send(images))
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const db = require("./db");
 const Image = require("./image/model");
 const imageRouter = require("./image/router");
 
 const app = express();
+
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+
+const parserMiddleware = bodyParser.json();
+app.use(parserMiddleware);
 
 app.use(imageRouter);
 
