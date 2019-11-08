353071a29d2af30773a81b5f5f0ea5dec99bf687
ahaigh9877 <alastairhaigh@me.com>
2019-11-08 09:33:52 +0100

    Added the database

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const db = require("./db");
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, console.log(`The app does listen: port number ${port}`));
+app.listen(
+  port,
+  console.log(`The app *does* listen: port number ${port} ! ! ! ! !`)
+);
